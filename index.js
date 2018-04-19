eaac0b638f194837396e3bff98905441a6fc8b08
Michael Jonker <michael@openpoint.ie>
2018-04-19 20:49:55 +0100

    index

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const common = {
+    Elastic:require('./lib/Elastic'),
+    Grpc:require('./lib/Grpc'),
+    Onion:require('./lib/Onion'),
+    System:require('./lib/System')
+}
+
+module.exports = common;
