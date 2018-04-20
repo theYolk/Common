d25a02195c7edda8318be936d123278ea493d62e
Michael Jonker <michael@openpoint.ie>
2018-04-20 15:48:16 +0100

    Add module discovery

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 "use strict";
 
-const common = {
-    Elastic:require('./lib/Elastic'),
-    Grpc:require('./lib/Grpc'),
-    Onion:require('./lib/Onion'),
-    System:require('./lib/System')
-}
+const fs = require('fs');
+const files = fs.readdirSync('./lib');
+const common = {};
+files.forEach((file)=>{
+    file = file.split('.')[0];
+    common[file] = require('./lib/'+file)
+})
 
 module.exports = common;
