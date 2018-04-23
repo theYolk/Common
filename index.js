1e361a3c9ca4966ded0082013cec13d44e275b39
Michael Jonker <michael@openpoint.ie>
2018-04-23 18:09:59 +0100

    Improved module exports

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 const fs = require('fs');
 const path = require('path');
 const files = fs.readdirSync(path.join(__dirname,'./lib'));
-const common = {};
+
 files.forEach((file)=>{
     file = file.split('.')[0];
-    common[file] = require(path.join(__dirname,'./lib',file))
+    module.exports[file] = require(path.join(__dirname,'./lib',file))
 })
-
-module.exports = common;
